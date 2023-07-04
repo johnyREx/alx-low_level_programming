e012ef79d9a5d11e5c943beb84a211a6fef3548e
johnyREx <johnkwofie99@icloud.com>
2023-07-04 12:45:01 -0700

    checker please be nice

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,9 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 1;
+	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
