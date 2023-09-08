da7acf1fb84dd92ca2a2b2e20fd30f890a377e8e
johnyREx <johnkwofie99@icloud.com>
2023-09-08 07:58:49 -0700

    they call me lil fish

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table to delete.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *current = ht->array[i];
+
+		while (current != NULL)
+		{
+			hash_node_t *next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
+	}
+
+	free(ht->array);
+
+	free(ht);
+}
