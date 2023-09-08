260f6e7284a25834429faf2ce25731d60881adb5
johnyREx <johnkwofie99@icloud.com>
2023-09-08 00:56:15 -0700

    made changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,8 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL;
 	unsigned long int i;
+	hash_table_t *new_table = NULL;
 
 	new_table = malloc(sizeof(hash_table_t));
 
